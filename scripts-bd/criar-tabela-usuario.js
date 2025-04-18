de919a93c7c67e10b44ba2d66292b6142e1b32d1
Ana Julia <anajuliadefendi@gmail.com>
2025-04-18 18:12:56 -0300

    Update criar-tabela-usuario.js

--- scripts-bd/criar-tabela-usuario.js
+++ scripts-bd/criar-tabela-usuario.js
@@ -1,21 +1,20 @@
 import { sql } from "../db.js"
 
-export async function criar(){
+export async function criar() {
     try {
         await sql`
-        CREATE TABLE usuario (
-            id_usuario SERIAL,
+        CREATE TABLE IF NOT EXISTS usuario (
+            id_usuario SERIAL PRIMARY KEY,
             registro_academico VARCHAR(30) UNIQUE,
             nome VARCHAR(100) NOT NULL,
             data_nascimento DATE,
             email VARCHAR(255),
             telefone VARCHAR(30),
-            id_tipo INTEGER REFERENCES tipo_usuario(id_tipo),
-            PRIMARY KEY(id_usuario)
+            id_tipo INTEGER REFERENCES tipo_usuario(id_tipo)
         );
-        `
-        console.log("Tabela criada")
+        `;
+        console.log("Tabela criada ou já existente");
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
